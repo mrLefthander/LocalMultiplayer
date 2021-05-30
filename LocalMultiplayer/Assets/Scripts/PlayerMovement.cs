67bdae9aef73e13030d1c37046fe7eb88344e35e
Lefthander <lefthander89@gmail.com>
2021-05-30 23:15:42 +0300

    jumping with new input system. ground check and prevent wall jumps

--- LocalMultiplayer/Assets/Scripts/PlayerMovement.cs
+++ LocalMultiplayer/Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
   [SerializeField]
   private float _movementSpeed = 10f;
   [SerializeField]
   private float _jumpForce = 21f;
+  [SerializeField]
+  private LayerMask groundLayerMask;
 
   private float _horizontalInput;
+  private bool _isGronded;
 
   private Rigidbody2D _rigidbody;
+  private CapsuleCollider2D _feetCollider;
 
   private void Start()
   {
     _rigidbody = GetComponent<Rigidbody2D>();
+    _feetCollider = GetComponent<CapsuleCollider2D>();
   }
 
-  private void Update()
+  private void FixedUpdate()
   {
     _rigidbody.velocity = new Vector2(_horizontalInput * _movementSpeed, _rigidbody.velocity.y);
-
-   /* if (Input.GetButtonDown("Jump"))
-    {
-      _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
-    }*/
+    _isGronded = IsGrounded();
   }
+
+  
 
   public void OnMoveInput(InputAction.CallbackContext context)
   {
     _horizontalInput = context.ReadValue<Vector2>().x;
+  }
+
+  public void OnJumpInput(InputAction.CallbackContext context)
+  {
+    if (!context.performed || !_isGronded) { return; }
+
+    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+  }
+
+  private bool IsGrounded()
+  {
+    return _feetCollider.IsTouchingLayers(groundLayerMask);
   }
 
 }
