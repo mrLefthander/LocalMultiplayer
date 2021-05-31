fe38049d2af8f494db0eda54cb749a370ec1cbd9
Lefthander <lefthander89@gmail.com>
2021-05-31 18:34:07 +0300

    Deleted 2nd collider on Player, changed IsGrounded logic, added SmallJump

--- LocalMultiplayer/Assets/Scripts/PlayerMovement.cs
+++ LocalMultiplayer/Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,44 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerMovement : MonoBehaviour
 {
+  private const float COLLIDER_SIZE_MULTIPLIER_FOR_GROUND_CHECK = 0.98f;
+  private const float BOX_CAST_ANGLE = 0f;
+  private const float SMALL_JUMP_MULTIPLIER = 0.3f;
+
   [SerializeField]
   private float _movementSpeed = 10f;
   [SerializeField]
   private float _jumpForce = 21f;
+
+
   [SerializeField]
   private LayerMask groundLayerMask;
-
-  private float _horizontalInput;
   private bool _isGronded;
+  private float _horizontalInput;
+
 
   private Rigidbody2D _rigidbody;
-  private CapsuleCollider2D _feetCollider;
+  private Collider2D _collider;
 
   private void Start()
   {
     _rigidbody = GetComponent<Rigidbody2D>();
-    _feetCollider = GetComponent<CapsuleCollider2D>();
+    _collider = GetComponent<Collider2D>();
   }
 
   private void FixedUpdate()
   {
-    _rigidbody.velocity = new Vector2(_horizontalInput * _movementSpeed, _rigidbody.velocity.y);
+    MoveHorizontaly();
     _isGronded = IsGrounded();
   }
 
-  
+  private void MoveHorizontaly()
+  {
+    _rigidbody.velocity = new Vector2(_horizontalInput * _movementSpeed, _rigidbody.velocity.y);
+  }
 
   public void OnMoveInput(InputAction.CallbackContext context)
   {
@@ -39,14 +48,33 @@ public class PlayerMovement : MonoBehaviour
 
   public void OnJumpInput(InputAction.CallbackContext context)
   {
-    if (!context.performed || !_isGronded) { return; }
+    Jump(context);
 
-    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+    SmallJump(context);
+  }
+
+  private void Jump(InputAction.CallbackContext context)
+  {
+    if (context.performed && _isGronded)
+    {
+      _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+    }
+  }
+
+  private void SmallJump(InputAction.CallbackContext context)
+  {
+    if (context.canceled && !_isGronded && _rigidbody.velocity.y > 0f)
+    {
+      _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * SMALL_JUMP_MULTIPLIER);
+    }
   }
 
   private bool IsGrounded()
   {
-    return _feetCollider.IsTouchingLayers(groundLayerMask);
+    RaycastHit2D raycastHit = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size * COLLIDER_SIZE_MULTIPLIER_FOR_GROUND_CHECK, BOX_CAST_ANGLE, 
+      Vector2.down, _collider.bounds.extents.x, groundLayerMask);
+
+    return raycastHit.collider != null;
   }
 
 }
