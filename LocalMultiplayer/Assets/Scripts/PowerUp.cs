6216356cc32e0a6eac6ccf1af958aad1f85e3e52
mrLefthander <lefthander89@gmail.com>
2021-07-16 23:01:09 +0300

    Added Invincibility and SpeedUp power ups GO and updated PowerUp script

--- LocalMultiplayer/Assets/Scripts/PowerUp.cs
+++ LocalMultiplayer/Assets/Scripts/PowerUp.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
   [SerializeField] private GameObject _powerUpEffect;
-  public bool isHealth;
+  [SerializeField] private float _powerUpTime;
+  [SerializeField] private float _powerUpPower;
+  [SerializeField] private bool _isHealth;
+  [SerializeField] private bool _isInvincible;
+  [SerializeField] private bool _isSpeed;
+  
 
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (ApplicationVariables.LayerNames.IsTouchingPlayer(other.gameObject.layer))
     {
-      if (isHealth)
+      if (_isHealth)
       {
         other.GetComponent<PlayerHealth>().ResetHealth();
+      }
+
+      if (_isInvincible)
+      {
+        other.GetComponent<PlayerHealth>().MakeInvincible(_powerUpTime);
+      }
+
+      if (_isSpeed)
+      {
+        other.GetComponent<PlayerMovement>().ChangeMovementSpeedForTime(_powerUpPower, _powerUpTime);
       }
 
       Instantiate(_powerUpEffect, transform.position, Quaternion.identity);
